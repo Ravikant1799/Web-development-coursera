a4d4eac73a956afb4cf897b4af7c1b6e1da7a0df
Ravikant <ravikantc17@gmail.com>
2020-07-22 20:17:26 +0530

    React redux form revist

--- React/confusion/src/components/ContactComponent.js
+++ React/confusion/src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button,  Label, Row, Col, FormFeedback} from 'reactstrap';
 import {Link} from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 
 const required = (val) => val && val.length;
@@ -21,7 +21,7 @@ class Contact extends Component{
     handleSubmit(values){
         console.log("Current state is: "+ JSON.stringify(values));
         alert("Current state is: "+ JSON.stringify(values));
-        //event.preventDefault();
+        this.props.resetFeedbackForm();
 
     }
 
@@ -72,7 +72,7 @@ class Contact extends Component{
                     <div className="col-12">
                         <h3>Send Us Your Feedback</h3>
                         <div class="col-12 col-md-9">
-                            <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+                            <Form model="feedback" onSubmit={(values)=>this.handleSubmit(values)}>
                                 <Row className="form-group">
                                     <Label htmlFor="firstname" md={2}>First Name</Label>
                                     <Col md={10}>
@@ -183,7 +183,7 @@ class Contact extends Component{
                                     </Col>
                                 </Row>
 
-                            </LocalForm>
+                            </Form>
                         </div>
                     </div>
                 </div>
