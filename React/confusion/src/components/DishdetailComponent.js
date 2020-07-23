c016ef5cc4583789a30235a23dd7de74d66492f3
Ravikant <ravikantc17@gmail.com>
2020-07-23 08:05:04 +0530

    Fetch from server

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { addComment } from '../redux/ActionCreators';
 import {Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 
     const required = (val) => val && val.length;
@@ -103,7 +104,7 @@ import {Loading } from './LoadingComponent';
         return(
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardImg width="100%" src={baseUrl+dish.image} alt={dish.name}/>
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
